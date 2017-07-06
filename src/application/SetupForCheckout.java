dab74d38df9babed794a37a8511f188641e3acf8
Samita Maharjan <samitamaharjan@gmail.com>
2017-07-06 07:43:22 -0500

    single setup file

--- src/application/SetupForCheckout.java
+++ src/application/SetupForCheckout.java
@@ -7,6 +7,7 @@ import business.Author;
 import business.Book;
 import business.BookCopy;
 import business.BookType;
+import business.CheckoutRecord;
 import business.LibraryMember;
 import dao.FileManager;
 import dao.FileManagerImpl;
@@ -14,10 +15,28 @@ import dao.FileManagerImpl;
 public class SetupForCheckout {
 
 	public static void main(String[] args) throws IOException {
-		FileManager<Book> bookManager = new FileManagerImpl<>("book");
-		 
-		bookManager.clean();
+		FileManager<CheckoutRecord> checkoutRecordManager = new FileManagerImpl<>("checkoutRecord");
+		checkoutRecordManager.clean();
+		
+		FileManager<LibraryMember> loginManager = new FileManagerImpl<>("libraryMember");
+		loginManager.clean();
 		Address addr1 = new Address("1000 N 4th St", "Fairfield", "52555", "IA");
+		Address addr2 = new Address("1260 N 4th St", "Seattle", "52555", "WA");
+		Address addr3 = new Address("2500 N 4th St", "Redmond", "52555", "PA");
+		LibraryMember maria = new LibraryMember("10", "Maria", "Jackson", "256981", addr1);
+		LibraryMember john = new LibraryMember("20", "John", "Smith", "2589642", addr2);
+		LibraryMember samita = new LibraryMember("30", "Samita", "Maharjan", "2058651", addr3);
+		LibraryMember madhuri = new LibraryMember("40", "Madhuri", "Agrawal", "2058651", addr3);
+		
+		loginManager.insert(john);
+		loginManager.insert(samita);
+		loginManager.insert(maria);
+		loginManager.insert(madhuri);
+		loginManager.print();
+		
+		FileManager<Book> bookManager = new FileManagerImpl<>("book");
+		bookManager.clean();
+		//Address addr1 = new Address("1000 N 4th St", "Fairfield", "52555", "IA");
 		Author author1 = new Author("Appollo", "Jason", "2589654", addr1, "cred", "bio");
 		BookCopy bookCopy1 = new BookCopy("1000");
 		Book java = new Book("1111", "Java", author1, bookCopy1, BookType.REFERENCE.toString());
@@ -30,7 +49,7 @@ public class SetupForCheckout {
 		python.addBookCopy(new BookCopy("2002"));
 		
 		BookCopy bookCopy3 = new BookCopy("3000");
-		Address addr2 = new Address("1250 N 4th St", "Fairfield", "52555", "IA");
+		//Address addr2 = new Address("1250 N 4th St", "Fairfield", "52555", "IA");
 		Author author2 = new Author("Alex", "Johnson", "2589654", addr2, "cred", "bio");
 		Book hadoop = new Book("3333", "Hadoop", author2, bookCopy3, BookType.REGULAR.toString());
 		
@@ -43,17 +62,5 @@ public class SetupForCheckout {
 		bookManager.insert(hadoop);
 		bookManager.insert(Ruby);
 		bookManager.print();
-		
-		/*FileManager<LibraryMember> memberManager = new FileManagerImpl<>("libraryMember");
-		memberManager.clean();
-		
-		LibraryMember john = new LibraryMember("1051", "John", "Smith", "2589642", null);
-		LibraryMember samita = new LibraryMember("1052", "Samita", "Maharjan", "2058651", null);
-		
-		memberManager.insert(john);
-		memberManager.insert(samita);
-		memberManager.print();
-		bookManager.print();*/
-		
 	}
 }
